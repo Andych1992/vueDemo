8ea737df4407cfa2f313e350c6543d1db35acc47
584531552 <584531552@qq.com>
2020-01-30 19:07:38 +0800

    更新组件

---
+++ pages/mainstroage/mainstroage.vue
@@ -0,0 +1,23 @@
+<!-- 这个文件我在做 我是群员(常州-_陈默 565036413) -->
+<template>
+	<view>
+		我是发放页面
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
