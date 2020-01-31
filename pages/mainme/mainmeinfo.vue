bbce0e9e4846e413243235cf754c2706fa37f637
cy <565036413@qq.com>
2020-01-31 14:43:39 +0800

    个人资料

---
+++ pages/mainme/mainmeinfo.vue
@@ -0,0 +1,26 @@
+<!-- 这个文件我在做 我是群员(常州-_陈默 565036413) -->
+<template>
+	<view>
+		<view class="uni-row u-f-ac">
+			基本信息
+		</view>
+		<uni-list>
+			<uni-list-item title="列表右侧带箭头" />
+			<uni-list-item title="列表右侧带箭头" />
+			<uni-list-item title="列表右侧带箭头" />
+			<uni-list-item title="列表右侧带箭头" />
+		</uni-list>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+	.uni-row {
+		border-bottom: #BEBEBE solid 1rpx;
+		height: 80rpx;
+		padding: 10rpx;
+		background-color: #E5E5E5;
+	}
+</style>
