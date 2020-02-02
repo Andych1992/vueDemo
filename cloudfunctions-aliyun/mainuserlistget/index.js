2f2c9ec2e1d3f25573842ce6740d3c9a39f8b7d9
cy <565036413@qq.com>
2020-02-02 14:01:19 +0800

    人员管理5

--- cloudfunctions-aliyun/mainuserlistget/index.js
+++ cloudfunctions-aliyun/mainuserlistget/index.js
@@ -1,7 +1,22 @@
 'use strict';
 const db = uniCloud.database()
 exports.main = async (event, context) => {
-  const collection = db.collection('user')
-  const res = await collection.get()
-  return res
+	const collection = db.collection('user');
+	if (event.searchKey != '') {
+		const dbCmd = db.command;
+		const res = await collection.where(
+			dbCmd.or(
+				{
+					name: new RegExp(event.searchKey)
+				}, 
+				{
+					section: new RegExp(event.searchKey)
+				})
+			).orderBy("create_time", "desc").skip((event.page - 1) * event.pageSize).limit(event.pageSize).get();
+		return res
+	} else {
+		const res = await collection
+			.orderBy("create_time", "desc").skip((event.page - 1) * event.pageSize).limit(event.pageSize).get();
+		return res
+	}
 };
